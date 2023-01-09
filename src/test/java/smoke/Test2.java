59b8275b747ac5dc200af83fd85bf094704c1c4e
Nick Nickulin <nikolay.nm11-3@yandex.ru>
2023-01-09 16:33:59 +0300

    third commit

--- src/test/java/smoke/Test2.java
+++ src/test/java/smoke/Test2.java
@@ -5,7 +5,14 @@ import org.testng.annotations.Test;
 public class Test2 {
 
     @Test(groups = {"smokeTest"})
-    public void test2() {
-        System.out.println("test2");
+    public void Test1() {
+        System.out.println("Posledovatelno test1");
+    }
+
+    @Test(dependsOnMethods = {"Test1"})
+    public void Test2() {
+        System.out.println("Posledovatelno test2");
     }
 }
+
+
